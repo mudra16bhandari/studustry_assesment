72331ab9fd7c067762af2e2077141009d54ce846
Mudra Bhandari <mudrabhandari123@yahoo.in>
2021-05-22 21:29:35 +0530

    SignUpPage UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:login_ui_studustry/screens/SignInPage.dart';
+import 'package:login_ui_studustry/screens/SignUpPage.dart';
 
 void main() {
   runApp(MyApp());
